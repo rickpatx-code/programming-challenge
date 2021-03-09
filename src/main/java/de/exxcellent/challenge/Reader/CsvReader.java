d3bbd9a2b91b0969b27b4f3d835f482bcf64bfee
rickpatx-code <Patrick PC@DESKTOP-DA7T8FJ.fritz.box>
2021-03-09 12:25:09 +0100

    + CsvReader
    + Implement Class "CsvReader" to read and transform Csv-Files

---
+++ src/main/java/de/exxcellent/challenge/Reader/CsvReader.java
@@ -0,0 +1,43 @@
+package de.exxcellent.challenge.Reader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.exxcellent.challenge.interfaces.Reader;
+
+/**
+ * Class to generate a List of String-Arrays out of a CSV-File
+ * 
+ * @author Patrick Gröber
+ *
+ */
+public class CsvReader implements Reader {
+
+	@Override
+	public List<String[]> read(String filepath) throws IOException {
+		
+		//read the file
+		BufferedReader br = new BufferedReader(new FileReader(filepath));		
+		
+		//init ArrayList
+		List<String[]> group = new ArrayList<String[]>();
+		
+		//read every line of the file
+		String line;
+		while ((line = br.readLine()) != null) {
+			
+			//split the current line at each "," and put every value into the StringArray
+			String[] columns = line.split(",");
+			
+			//add the StringArray to the ArrayList
+			group.add(columns);
+		}
+		
+		//return the ArrayList
+		return group;
+	}
+
+}
